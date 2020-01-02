8058d18f29af56307d566a6dae06ea6aa8084344
gong qiu <gong qiu>
2020-01-02 18:33:21 +0800

    [CHG] init

---
+++ artisticprobes/src/main/java/com/qiugong/artisticprobes/x01/MainActivity.java
@@ -0,0 +1,15 @@
+package com.qiugong.artisticprobes.x01;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.qiugong.artisticprobes.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
