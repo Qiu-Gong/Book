5dbc9bbc3af4726f5dfb0559a6544103592a5f24
gong qiu <gong qiu>
2020-01-10 21:57:44 +0800

    [CHG] x03- 子容器拦截

---
+++ artisticprobes/src/main/java/com/qiugong/artisticprobes/x03/child/ChildActivity.java
@@ -0,0 +1,72 @@
+package com.qiugong.artisticprobes.x03.child;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.qiugong.artisticprobes.R;
+import com.qiugong.artisticprobes.x03.Utils;
+
+import java.util.ArrayList;
+
+
+/**
+ * @author qzx 2019/5/23.
+ */
+public class ChildActivity extends Activity {
+
+    private static final String TAG = "DemoActivity_1";
+
+    private HorizontalScrollViewEx2 mListContainer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_child);
+        Log.d(TAG, "onCreate");
+        initView();
+    }
+
+    private void initView() {
+        LayoutInflater inflater = getLayoutInflater();
+        mListContainer = findViewById(R.id.container);
+        final int screenWidth = Utils.getScreenMetrics(this).widthPixels;
+        final int screenHeight = Utils.getScreenMetrics(this).heightPixels;
+
+        for (int i = 0; i < 3; i++) {
+            ViewGroup layout = (ViewGroup) inflater.inflate(R.layout.content_layout2, mListContainer, false);
+            layout.getLayoutParams().width = screenWidth;
+            TextView textView = layout.findViewById(R.id.title);
+            textView.setText("page " + (i + 1));
+            layout.setBackgroundColor(Color.rgb(255 / (i + 1), 255 / (i + 1), 0));
+            createList(layout);
+            mListContainer.addView(layout);
+        }
+    }
+
+    private void createList(ViewGroup layout) {
+        ListViewEx listView = layout.findViewById(R.id.list);
+        listView.setHorizontalScrollViewEx2(mListContainer);
+        ArrayList<String> datas = new ArrayList<>();
+        for (int i = 0; i < 50; i++) {
+            datas.add("name " + i);
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.content_list_item, R.id.name, datas);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(ChildActivity.this, "click item", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
---
+++ artisticprobes/src/main/java/com/qiugong/artisticprobes/x03/child/ListViewEx.java
@@ -0,0 +1,82 @@
+package com.qiugong.artisticprobes.x03.child;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.widget.ListView;
+
+/**
+ * @author qzx 2019/5/24.
+ */
+public class ListViewEx extends ListView {
+
+    private static final String TAG = "ListViewEx";
+
+    private HorizontalScrollViewEx2 mHorizontalScrollViewEx2;
+
+    // 分别记录上次滑动的坐标
+    private int mLastX = 0;
+    private int mLastY = 0;
+
+    public ListViewEx(Context context) {
+        super(context);
+    }
+
+    public ListViewEx(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ListViewEx(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public void setHorizontalScrollViewEx2(HorizontalScrollViewEx2 horizontalScrollViewEx2) {
+        mHorizontalScrollViewEx2 = horizontalScrollViewEx2;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                Log.d(TAG, "ACTION_DOWN");
+            case MotionEvent.ACTION_MOVE:
+                Log.d(TAG, "ACTION_MOVE");
+            case MotionEvent.ACTION_UP:
+                Log.d(TAG, "ACTION_UP");
+        }
+        return super.onTouchEvent(ev);
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        int x = (int) event.getX();
+        int y = (int) event.getY();
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN: {
+                mHorizontalScrollViewEx2.requestDisallowInterceptTouchEvent(true);
+                break;
+            }
+            case MotionEvent.ACTION_MOVE: {
+                int deltaX = x - mLastX;
+                int deltaY = y - mLastY;
+                if (Math.abs(deltaX) > Math.abs(deltaY)) {
+                    Log.d(TAG, "ListViewEx deltaX:" + deltaX + " deltaY:" + deltaY);
+                    // 父类需要此点击事件
+                    mHorizontalScrollViewEx2.requestDisallowInterceptTouchEvent(false);
+                }
+                break;
+            }
+            case MotionEvent.ACTION_UP: {
+                break;
+            }
+            default:
+                break;
+        }
+
+        mLastX = x;
+        mLastY = y;
+        return super.dispatchTouchEvent(event);
+    }
+}
