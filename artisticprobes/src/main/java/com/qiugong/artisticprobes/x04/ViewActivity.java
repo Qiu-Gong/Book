8a843031c9a56fe56f09df6d46e30c42b0b6d45c
gong qiu <gong qiu>
2020-01-11 01:28:11 +0800

    [CHG] x04- 自定义View

---
+++ artisticprobes/src/main/java/com/qiugong/artisticprobes/x04/ViewActivity.java
@@ -0,0 +1,18 @@
+package com.qiugong.artisticprobes.x04;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.qiugong.artisticprobes.R;
+
+/**
+ * @author qzx 2019/6/2.
+ */
+public class ViewActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view);
+    }
+}
