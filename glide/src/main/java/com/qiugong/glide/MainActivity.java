a351fcde476793e7933343cb9de418df5f206b8b
gong qiu <gong qiu>
2020-02-20 00:02:30 +0800

    [CHG] 新增 SecondActivity

--- glide/src/main/java/com/qiugong/glide/MainActivity.java
+++ glide/src/main/java/com/qiugong/glide/MainActivity.java
@@ -1,7 +1,9 @@
 package com.qiugong.glide;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void entry(View view) {
+        startActivity(new Intent(this, SecondActivity.class));
     }
 }
