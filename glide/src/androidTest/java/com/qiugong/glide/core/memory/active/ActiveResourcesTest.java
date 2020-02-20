534f3c7df01a6bdb95a4f42742b6fb359e2fe5d2
gong qiu <gong qiu>
2020-02-20 16:28:31 +0800

    [CHG] ActiveResources 资源

---
+++ glide/src/androidTest/java/com/qiugong/glide/core/memory/active/ActiveResourcesTest.java
@@ -0,0 +1,73 @@
+package com.qiugong.glide.core.memory.active;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import com.qiugong.glide.R;
+import com.qiugong.glide.core.memory.Key;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author qzx 20/2/20.
+ */
+@RunWith(AndroidJUnit4.class)
+public class ActiveResourcesTest {
+
+    class Active {
+        Bitmap bitmap;
+        Resource resource;
+        Key key;
+
+        public Active(Resources resources, int id) {
+            bitmap = BitmapFactory.decodeResource(resources, id);
+            resource = new Resource(bitmap);
+            key = new Key() {
+            };
+        }
+
+        @NonNull
+        @Override
+        public String toString() {
+            return "bitmap:" + bitmap + " resource:" + resource + " key:" + key;
+        }
+    }
+
+    @Test
+    private void testActiveResources() {
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        Active a1 = new Active(appContext.getResources(), R.mipmap.ic_launcher);
+        Active a2 = new Active(appContext.getResources(), R.mipmap.ic_launcher);
+        Active a3 = new Active(appContext.getResources(), R.mipmap.ic_launcher);
+
+        ActiveResources resources = new ActiveResources(new Resource.ResourceListener() {
+            @Override
+            public void onResourceReleased(Key key, Resource resource) {
+                Log.d("TEST", "onResourceReleased key:" + key + " resource:" + resource);
+            }
+        });
+        resources.activate(a1.key, a1.resource);
+        resources.activate(a2.key, a2.resource);
+        resources.activate(a3.key, a3.resource);
+
+        Log.d("TEST", "a1:" + resources.get(a1.key)
+                + " a2:" + resources.get(a2.key)
+                + " a3:" + resources.get(a3.key));
+
+        resources.deactivate(a1.key);
+        resources.deactivate(a2.key);
+        resources.deactivate(a3.key);
+
+        System.gc();
+
+        resources.shutDown();
+    }
+}
---
+++ glide/src/main/java/com/qiugong/glide/core/memory/active/ActiveResources.java
@@ -0,0 +1,104 @@
+package com.qiugong.glide.core.memory.active;
+
+import android.util.Log;
+
+import com.qiugong.glide.core.memory.Key;
+
+import java.lang.ref.ReferenceQueue;
+import java.lang.ref.WeakReference;
+import java.util.Map;
+import java.util.WeakHashMap;
+
+/**
+ * TODO 弱引用如何释放资源
+ *
+ * @author qzx 20/2/20.
+ */
+public class ActiveResources {
+
+    private static final String TAG = "ActiveResources";
+
+    private Resource.ResourceListener resourceListener;
+
+    private Map<Key, ResourceWeakReference> activeResources = new WeakHashMap<>();
+    private ReferenceQueue<Resource> referenceQueue;
+    private Thread cleanReferenceQueueThread;
+    private volatile boolean threadShutDown;
+
+    public ActiveResources(Resource.ResourceListener listener) {
+        this.resourceListener = listener;
+    }
+
+    public void activate(Key key, Resource resource) {
+        Log.d(TAG, "activate key:" + key + " resource:" + resource);
+        resource.setResourceListener(key, resourceListener);
+        activeResources.put(key, new ResourceWeakReference(key, resource, getReferenceQueue()));
+    }
+
+    public void deactivate(Key key) {
+        Log.d(TAG, "deactivate key:" + key);
+        ResourceWeakReference resource = activeResources.remove(key);
+        if (resource != null) {
+            resource.clear();
+        }
+    }
+
+    public Resource get(Key key) {
+        Log.d(TAG, "get key:" + key);
+        ResourceWeakReference resource = activeResources.get(key);
+        if (resource != null) {
+            return resource.get();
+        }
+        return null;
+    }
+
+    public void shutDown() {
+        Log.d(TAG, "shutDown");
+        threadShutDown = true;
+        if (cleanReferenceQueueThread == null) {
+            return;
+        }
+
+        try {
+            cleanReferenceQueueThread.interrupt();
+            cleanReferenceQueueThread.join(5);
+            if (cleanReferenceQueueThread.isAlive()) {
+                throw new RuntimeException("clean Reference Queue Thread failed shut down.");
+            }
+        } catch (InterruptedException e) {
+        }
+    }
+
+    private ReferenceQueue<? super Resource> getReferenceQueue() {
+        if (referenceQueue == null) {
+            referenceQueue = new ReferenceQueue<>();
+            cleanReferenceQueueThread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    Log.d(TAG, "run...");
+                    while (!threadShutDown) {
+                        try {
+                            ResourceWeakReference resource = (ResourceWeakReference) referenceQueue.remove();
+                            Log.d(TAG, "remove active resources. key:" + resource.key);
+                            activeResources.remove(resource.key);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }, "active-resources");
+            cleanReferenceQueueThread.start();
+        }
+        return referenceQueue;
+    }
+
+    private static class ResourceWeakReference extends WeakReference<Resource> {
+
+        private Key key;
+
+        public ResourceWeakReference(Key key, Resource resource,
+                                     ReferenceQueue<? super Resource> queue) {
+            super(resource, queue);
+        }
+    }
+}
