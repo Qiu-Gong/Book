2d9cf04adc19806bd98beab5c1c4bfabed74d8b7
Qiu-Gong <178438297@qq.com>
2020-08-16 22:42:43 +0800

    [CHG] 04-简单工厂

---
+++ head_first/src/main/java/com/qiugong/first/x04_factory/simple/ClamPizza.java
@@ -0,0 +1,11 @@
+package com.qiugong.first.x04_factory.simple;
+
+public class ClamPizza extends Pizza {
+	public ClamPizza() {
+		name = "Clam Pizza";
+		dough = "Thin crust";
+		sauce = "White garlic sauce";
+		toppings.add("Clams");
+		toppings.add("Grated parmesan cheese");
+	}
+}
