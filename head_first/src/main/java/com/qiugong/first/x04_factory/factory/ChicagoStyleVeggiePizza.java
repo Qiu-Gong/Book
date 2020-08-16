c3387af1911f53706a1d672115e7b54ac5412b82
Qiu-Gong <178438297@qq.com>
2020-08-16 23:57:35 +0800

    [CHG] 04-工厂模式

---
+++ head_first/src/main/java/com/qiugong/first/x04_factory/factory/ChicagoStyleVeggiePizza.java
@@ -0,0 +1,18 @@
+package com.qiugong.first.x04_factory.factory;
+
+public class ChicagoStyleVeggiePizza extends Pizza {
+	public ChicagoStyleVeggiePizza() {
+		name = "Chicago Deep Dish Veggie Pizza";
+		dough = "Extra Thick Crust Dough";
+		sauce = "Plum Tomato Sauce";
+ 
+		toppings.add("Shredded Mozzarella Cheese");
+		toppings.add("Black Olives");
+		toppings.add("Spinach");
+		toppings.add("Eggplant");
+	}
+ 
+	void cut() {
+		System.out.println("Cutting the pizza into square slices");
+	}
+}
---
+++ head_first/src/main/java/com/qiugong/first/x04_factory/factory/NYStyleVeggiePizza.java
@@ -0,0 +1,16 @@
+package com.qiugong.first.x04_factory.factory;
+
+public class NYStyleVeggiePizza extends Pizza {
+
+	public NYStyleVeggiePizza() {
+		name = "NY Style Veggie Pizza";
+		dough = "Thin Crust Dough";
+		sauce = "Marinara Sauce";
+ 
+		toppings.add("Grated Reggiano Cheese");
+		toppings.add("Garlic");
+		toppings.add("Onion");
+		toppings.add("Mushrooms");
+		toppings.add("Red Pepper");
+	}
+}
