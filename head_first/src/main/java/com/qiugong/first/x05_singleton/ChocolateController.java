9de572bddbcdcd76222a3b20ac4fce79e17453d5
Qiu-Gong <178438297@qq.com>
2020-08-17 22:45:41 +0800

    [CHG] 05-简单单例

---
+++ head_first/src/main/java/com/qiugong/first/x05_singleton/ChocolateController.java
@@ -0,0 +1,13 @@
+package com.qiugong.first.x05_singleton;
+ 
+public class ChocolateController {
+	public static void main(String args[]) {
+		ChocolateBoiler boiler = ChocolateBoiler.getInstance();
+		boiler.fill();
+		boiler.boil();
+		boiler.drain();
+
+		// will return the existing instance
+		ChocolateBoiler boiler2 = ChocolateBoiler.getInstance();
+	}
+}
