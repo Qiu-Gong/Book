b48c2991a417193cf040288fe1fa81a1b8209c62
Qiu-Gong <178438297@qq.com>
2020-08-25 06:16:13 +0800

    [CHG] 08-模版方法

---
+++ head_first/src/main/java/com/qiugong/first/x08_templatemethod/Coffee.java
@@ -0,0 +1,10 @@
+package com.qiugong.first.x08_templatemethod;
+
+public class Coffee extends CaffeineBeverage {
+	public void brew() {
+		System.out.println("Dripping Coffee through filter");
+	}
+	public void addCondiments() {
+		System.out.println("Adding Sugar and Milk");
+	}
+}
