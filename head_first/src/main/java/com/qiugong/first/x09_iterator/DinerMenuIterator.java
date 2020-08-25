e43e7c806f9311b2eb89ed044bfdc8a955f776d2
Qiu-Gong <178438297@qq.com>
2020-08-26 06:37:45 +0800

    [CHG] 09-使用java迭代器

--- head_first/src/main/java/com/qiugong/first/x09_iterator/DinerMenuIterator.java
+++ head_first/src/main/java/com/qiugong/first/x09_iterator/DinerMenuIterator.java
@@ -10,17 +10,32 @@ public class DinerMenuIterator implements Iterator {
         this.items = items;
     }
 
+    @Override
     public MenuItem next() {
         MenuItem menuItem = items[position];
         position = position + 1;
         return menuItem;
     }
 
+    @Override
     public boolean hasNext() {
         if (position >= items.length || items[position] == null) {
             return false;
         } else {
             return true;
+        }
+    }
+
+    @Override
+    public void remove() {
+        if (position < 0) {
+            throw new IllegalStateException("You can't remove an item until you've done at least one next()");
+        }
+        if (items[position - 1] != null) {
+            for (int i = position - 1; i < (items.length - 1); i++) {
+                items[i] = items[i + 1];
+            }
+            items[items.length - 1] = null;
         }
     }
 }
--- head_first/src/main/java/com/qiugong/first/x09_iterator/PancakeHouseMenu.java
+++ head_first/src/main/java/com/qiugong/first/x09_iterator/PancakeHouseMenu.java
@@ -42,7 +42,7 @@ public class PancakeHouseMenu implements Menu {
 	}
   
 	public Iterator createIterator() {
-		return new PancakeHouseMenuIterator(menuItems);
+		return menuItems.iterator();
 	}
   
 	public String toString() {
