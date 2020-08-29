7f3e76965b54c743e08e50311f82eff49c7ff7bf
Qiu-Gong <178438297@qq.com>
2020-08-30 06:03:38 +0800

    [CHG] 11-提交gumball

---
+++ head_first/src/main/java/com/qiugong/first/x11_proxy/gumball/State.java
@@ -0,0 +1,14 @@
+package com.qiugong.first.x11_proxy.gumball;
+
+public interface State {
+
+    public void insertQuarter();
+
+    public void ejectQuarter();
+
+    public void turnCrank();
+
+    public void dispense();
+
+    public void refill();
+}
