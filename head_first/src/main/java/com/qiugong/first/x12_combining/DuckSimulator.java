6c425c172710bae1e2d998f188de813806c34522
Qiu-Gong <178438297@qq.com>
2020-08-30 21:58:59 +0800

    [CHG] 12-组合模式

--- head_first/src/main/java/com/qiugong/first/x12_combining/DuckSimulator.java
+++ head_first/src/main/java/com/qiugong/first/x12_combining/DuckSimulator.java
@@ -2,6 +2,7 @@ package com.qiugong.first.x12_combining;
 
 import com.qiugong.first.x12_combining.adapter.Goose;
 import com.qiugong.first.x12_combining.adapter.GooseAdapter;
+import com.qiugong.first.x12_combining.composite.Flock;
 import com.qiugong.first.x12_combining.decorator.QuackCounter;
 import com.qiugong.first.x12_combining.ducks.Quackable;
 import com.qiugong.first.x12_combining.factory.AbstractDuckFactory;
@@ -15,19 +16,34 @@ public class DuckSimulator {
     }
 
     void simulate(AbstractDuckFactory duckFactory) {
-        Quackable mallardDuck = duckFactory.createMallardDuck();
         Quackable redheadDuck = duckFactory.createRedheadDuck();
         Quackable duckCall = duckFactory.createDuckCall();
         Quackable rubberDuck = duckFactory.createRubberDuck();
         Quackable gooseDuck = new GooseAdapter(new Goose());
+        System.out.println("\nDuck Simulator: With Composite - Flocks");
 
-        System.out.println("\nDuck Simulator");
+        Flock flockOfDucks = new Flock();
+        flockOfDucks.add(redheadDuck);
+        flockOfDucks.add(duckCall);
+        flockOfDucks.add(rubberDuck);
+        flockOfDucks.add(gooseDuck);
 
-        simulate(mallardDuck);
-        simulate(redheadDuck);
-        simulate(duckCall);
-        simulate(rubberDuck);
-        simulate(gooseDuck);
+        Quackable mallardOne = duckFactory.createMallardDuck();
+        Quackable mallardTwo = duckFactory.createMallardDuck();
+        Quackable mallardThree = duckFactory.createMallardDuck();
+        Quackable mallardFour = duckFactory.createMallardDuck();
+        Flock flockOfMallards = new Flock();
+        flockOfMallards.add(mallardOne);
+        flockOfMallards.add(mallardTwo);
+        flockOfMallards.add(mallardThree);
+        flockOfMallards.add(mallardFour);
+
+        flockOfDucks.add(flockOfMallards);
+
+        System.out.println("\nDuck Simulator: Whole Flock Simulation");
+        simulate(flockOfDucks);
+        System.out.println("\nDuck Simulator: Mallard Flock Simulation");
+        simulate(flockOfMallards);
 
         System.out.println("The ducks quacked " + QuackCounter.getQuacks() + " times");
     }
